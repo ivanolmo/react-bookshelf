ca608efc8210c9a7834036dad49719aeb7e6421c
Ivan Olmo <ivan.olmo@gmail.com>
2021-09-12 14:31:36 -0500

    Add starter BookList component. Will probably refactor shelves into components too

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import * as BooksAPI from './BooksAPI';
-import Book from './Book.js';
+import BookList from './BookList.js';
+
 import './App.css';
+import Book from './Book';
 
 function App() {
   // state = {
@@ -60,50 +62,7 @@ function App() {
 
   return (
     <div className='app'>
-      <div className='list-books'>
-        <div className='list-books-title'>
-          <h1>MyReads</h1>
-        </div>
-        <div className='list-books-content'>
-          <div>
-            <div className='bookshelf'>
-              <h2 className='bookshelf-title'>Currently Reading</h2>
-              <div className='bookshelf-books'>
-                <ol className='books-grid'>
-                  <li>
-                    <Book />
-                  </li>
-                </ol>
-              </div>
-            </div>
-            <div className='bookshelf'>
-              <h2 className='bookshelf-title'>Want to Read</h2>
-              <div className='bookshelf-books'>
-                <ol className='books-grid'>
-                  <li>
-                    <Book />
-                  </li>
-                </ol>
-              </div>
-            </div>
-            <div className='bookshelf'>
-              <h2 className='bookshelf-title'>Read</h2>
-              <div className='bookshelf-books'>
-                <ol className='books-grid'>
-                  <li>
-                    <Book />
-                  </li>
-                </ol>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='open-search'>
-          <button onClick={() => console.log('search clicked')}>
-            Add a book
-          </button>
-        </div>
-      </div>
+      <BookList />
     </div>
   );
 }
---
+++ src/BookList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function BookList() {
+  return (
+    <div className='list-books'>
+      <div className='list-books-title'>
+        <h1>MyReads</h1>
+      </div>
+      <div className='list-books-content'>
+        <div>
+          <div className='bookshelf'>
+            <h2 className='bookshelf-title'>Currently Reading</h2>
+            <div className='bookshelf-books'>
+              <ol className='books-grid'>
+                <li>{/* Book Component */}</li>
+              </ol>
+            </div>
+          </div>
+          <div className='bookshelf'>
+            <h2 className='bookshelf-title'>Want to Read</h2>
+            <div className='bookshelf-books'>
+              <ol className='books-grid'>
+                <li>{/* Book Component */}</li>
+              </ol>
+            </div>
+          </div>
+          <div className='bookshelf'>
+            <h2 className='bookshelf-title'>Read</h2>
+            <div className='bookshelf-books'>
+              <ol className='books-grid'>
+                <li>{/* Book Component */}</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='open-search'>
+        <button onClick={() => console.log('search clicked')}>
+          Add a book
+        </button>
+      </div>
+    </div>
+  );
+}
