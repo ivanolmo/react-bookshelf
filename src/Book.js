adcd64684db5a5837d9138864dba874fe951db60
Ivan Olmo <ivan.olmo@gmail.com>
2021-09-13 16:57:14 -0500

    Update Book component. Now destructures props and correctly displays thumbnail, title, and author(s).

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './App.css';
 
-export default function Book() {
+export default function Book({ book }) {
+  const { title, authors, imageLinks } = book;
   return (
     <li>
       <div className='book'>
         <div className='book-top'>
+          <div
+            className='book-cover'
+            style={{
+              width: '128px',
+              height: '193px',
+              backgroundImage: `url(${imageLinks && imageLinks.thumbnail})`,
+            }}
+            alt='book-cover'
+          />
           <div className='book-shelf-changer'>
             <select>
               <option value='move' disabled>
@@ -17,8 +28,10 @@ export default function Book() {
             </select>
           </div>
         </div>
-        <div className='book-title'>Placeholder Title</div>
-        <div className='book-authors'>Placeholder Author</div>
+        <div className='book-title'>{title}</div>
+        <div className='book-authors'>
+          {authors.length === 1 ? `${authors[0]}` : `${authors.join(', ')}`}
+        </div>
       </div>
     </li>
   );
