370cd57f1eac839f90ad100c1c61e56423e3675a
Ivan Olmo <ivan.olmo@gmail.com>
2021-09-15 15:05:17 -0500

    Update Search component. Add Book component to render function. Add necessary props. Update search logic.

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { DebounceInput } from 'react-debounce-input';
 
-export default function Search({ searchBooks }) {
+import Book from './Book';
+
+export default function Search({
+  searchedBooksList,
+  searchBooks,
+  updateShelf,
+}) {
   // state variable to store and update query as it is typed
   const [query, setQuery] = useState('');
 
@@ -17,10 +23,15 @@ export default function Search({ searchBooks }) {
         </Link>
         <div
           className='search-books-input-wrapper'
-          // add useEffect hook so that state sent to API
+          // add useEffect hook so that state sent to API matches input field
+          // because setState hook is asynchronous. also pass in state variable
+          // query as the optional second arg so re-render only occurs as state
+          // changes.
           onChange={useEffect(
             () => {
-              searchBooks(query);
+              if (query) {
+                searchBooks(query);
+              }
             },
             [query]
           )}
@@ -34,8 +45,18 @@ export default function Search({ searchBooks }) {
           />
         </div>
       </div>
-      <div className='search-books-results'>
-        <ol className='books-grid' />
+      <div className='bookshelf'>
+        <div className='bookshelf-books'>
+          <div className='search-books-results'>
+            <ol className='books-grid'>
+              {searchedBooksList.map((book) => {
+                return (
+                  <Book key={book.id} book={book} updateShelf={updateShelf} />
+                );
+              })}
+            </ol>
+          </div>
+        </div>
       </div>
     </div>
   );
